0970b30e6759cf076a8e43cb4ba72d96bc818186
Nikita Basargin <nikita.basargin@dlr.de>
2023-05-17 16:26:26 +0200

    h_a_alpha decomposition

---
+++ pypolinsar/polsar/decompositions.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def h_a_alpha_decomposition(coherency_matrix):
+    """Entropy/Anisotropy/Alpha (H/A/alpha) decomposition of a 3x3 polarimetric coherency matrix.
+    Both individual matrices ((3, 3) array) and batches of matrices ((..., 3, 3) array) are supported.
+
+    Arguments:
+        coherency_matrix -- (..., 3, 3) array with polarimetric coherency matrices
+
+    Returns:
+        A tuple of entropy, anisotropy, mean alpha angle, and dominant alpha angle.
+        In case an individual 3x3 matrix is decomposed, the tuple contains scalar values.
+        In the batched case, the tuple contains arrays that have the shape of the batch dimensions.
+    
+    Reference: 
+        Cloude, Shane R., and Eric Pottier. "A Review of Target Decomposition Theorems in Radar Polarimetry."
+    """
+    t_eigval, t_eigvec = np.linalg.eigh(coherency_matrix)
+    pseudo_probs = t_eigval / np.sum(t_eigval, axis=-1, keepdims=True)
+    # alpha angles
+    alpha_i = np.arccos(np.abs(t_eigvec[..., 0])) # arccos of abs of first element of each eigenvector
+    alpha_mean = np.sum(pseudo_probs * alpha_i, axis=-1) # average alpha angle (weighted by eigenvalues)
+    alpha_dominant = alpha_i[..., 2] # alpha angle corresponding to the largest eigenvalue (at index 2)
+    # entropy
+    log_pseudo_probs = np.log(pseudo_probs, out=np.zeros_like(pseudo_probs), where=pseudo_probs > 1e-5)
+    entropy = -np.sum(pseudo_probs * log_pseudo_probs, axis=-1) / np.log(3) # log base 3
+    # anisotropy
+    t_eigval_2 = t_eigval[..., 1] # second (intermediate) eigenvalue has index 1
+    t_eigval_3 = t_eigval[..., 0] # lthird (smallest) has index 0 (ascending sorting)
+    t_eigval_sum = t_eigval_2 + t_eigval_3
+    anisotropy = np.divide(t_eigval_2 - t_eigval_3, t_eigval_sum, out=np.zeros_like(t_eigval_sum), where=t_eigval_sum!=0)
+    return entropy, anisotropy, alpha_mean, alpha_dominant
+
